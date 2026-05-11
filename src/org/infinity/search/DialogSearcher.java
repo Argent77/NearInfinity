48c46f62b01ee7ec5c2844ca0d1509d8f42f26e7
Argent77 <4519923+Argent77@users.noreply.github.com>
2026-05-11 22:55:47 +0200

    DLG Text Search: Add option to toggle search in state and response text
    Turning it off allows to search explicitly only in dialog script code.

--- src/org/infinity/search/DialogSearcher.java
+++ src/org/infinity/search/DialogSearcher.java
@@ -47,13 +47,14 @@ import org.infinity.util.Logger;
 
 public final class DialogSearcher extends AbstractSearcher implements Runnable, ActionListener {
   private final ChildFrame inputFrame;
-  private final JButton bsearch = new JButton("Search", Icons.ICON_FIND_AGAIN_16.getIcon());
-  private final JCheckBox cbwhole = new JCheckBox("Match whole word only");
-  private final JCheckBox cbcase = new JCheckBox("Match case");
-  private final JCheckBox cbsearchcode = new JCheckBox("Include trigger & action scripts", true);
-  private final JCheckBox cbregex = new JCheckBox("Use regular expressions");
-  private final JLabel lregexHelp = ViewerUtil.createRegexpHelpLabel();
-  private final JTextField tfinput = new JTextField("", 15);
+  private final JButton bSearch = new JButton("Search", Icons.ICON_FIND_AGAIN_16.getIcon());
+  private final JCheckBox cbWhole = new JCheckBox("Match whole word only");
+  private final JCheckBox cbCase = new JCheckBox("Match case");
+  private final JCheckBox cbSearchText = new JCheckBox("Include state & response text", true);
+  private final JCheckBox cbSearchCode = new JCheckBox("Include trigger & action scripts", true);
+  private final JCheckBox cbRegex = new JCheckBox("Use regular expressions");
+  private final JLabel lRegexHelp = ViewerUtil.createRegexpHelpLabel();
+  private final JTextField tfInput = new JTextField("", 15);
   private final List<ResourceEntry> files;
 
   private Pattern regPattern;
@@ -68,31 +69,33 @@ public final class DialogSearcher extends AbstractSearcher implements Runnable, 
     }
     inputFrame = new ChildFrame(title, true);
     inputFrame.setIconImage(Icons.ICON_FIND_16.getIcon().getImage());
-    inputFrame.getRootPane().setDefaultButton(bsearch);
-    bsearch.addActionListener(this);
-    tfinput.addActionListener(this);
+    inputFrame.getRootPane().setDefaultButton(bSearch);
+    bSearch.addActionListener(this);
+    tfInput.addActionListener(this);
 
     JPanel regexpPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 0));
-    regexpPanel.add(cbregex);
-    regexpPanel.add(lregexHelp);
+    regexpPanel.add(cbRegex);
+    regexpPanel.add(lRegexHelp);
 
     Container pane = inputFrame.getContentPane();
     GridBagLayout gbl = new GridBagLayout();
     GridBagConstraints gbc = new GridBagConstraints();
     pane.setLayout(gbl);
     JLabel label = new JLabel("Find what:");
-    label.setLabelFor(tfinput);
+    label.setLabelFor(tfInput);
     label.setDisplayedMnemonic('f');
 
     JPanel matchpanel = new JPanel();
-    matchpanel.setLayout(new GridLayout(2, 2));
-    matchpanel.add(cbwhole);
-    matchpanel.add(cbsearchcode);
-    matchpanel.add(cbcase);
+    matchpanel.setLayout(new GridLayout(0, 2));
+    matchpanel.add(cbWhole);
+    matchpanel.add(cbSearchText);
+    matchpanel.add(cbCase);
+    matchpanel.add(cbSearchCode);
     matchpanel.add(regexpPanel);
-    cbwhole.setMnemonic('w');
-    cbcase.setMnemonic('m');
-    cbregex.setMnemonic('r');
+    matchpanel.add(new JLabel()); // empty placeholder
+    cbWhole.setMnemonic('w');
+    cbCase.setMnemonic('m');
+    cbRegex.setMnemonic('r');
 
     gbc.insets = new Insets(6, 6, 3, 3);
     gbc.weightx = 0.0;
@@ -105,14 +108,14 @@ public final class DialogSearcher extends AbstractSearcher implements Runnable, 
     gbc.weightx = 1.0;
     gbc.gridwidth = 2;
     gbc.insets.left = 3;
-    gbl.setConstraints(tfinput, gbc);
-    pane.add(tfinput);
+    gbl.setConstraints(tfInput, gbc);
+    pane.add(tfInput);
 
     gbc.weightx = 0.0;
     gbc.insets.right = 6;
     gbc.gridwidth = GridBagConstraints.REMAINDER;
-    gbl.setConstraints(bsearch, gbc);
-    pane.add(bsearch);
+    gbl.setConstraints(bSearch, gbc);
+    pane.add(bSearch);
 
     gbc.gridwidth = GridBagConstraints.REMAINDER;
     gbc.insets = new Insets(3, 6, 6, 6);
@@ -128,7 +131,7 @@ public final class DialogSearcher extends AbstractSearcher implements Runnable, 
 
   @Override
   public void actionPerformed(ActionEvent event) {
-    if (event.getSource() == bsearch || event.getSource() == tfinput) {
+    if (event.getSource() == bSearch || event.getSource() == tfInput) {
       inputFrame.setVisible(false);
       new Thread(this).start();
     }
@@ -140,16 +143,16 @@ public final class DialogSearcher extends AbstractSearcher implements Runnable, 
 
   @Override
   public void run() {
-    String term = tfinput.getText();
-    if (!cbregex.isSelected()) {
+    String term = tfInput.getText();
+    if (!cbRegex.isSelected()) {
       term = Pattern.quote(term);
     }
-    if (cbwhole.isSelected()) {
+    if (cbWhole.isSelected()) {
       term = "\\b" + term + "\\b";
     }
 
     try {
-      if (cbcase.isSelected()) {
+      if (cbCase.isSelected()) {
         regPattern = Pattern.compile(term, Pattern.DOTALL);
       } else {
         regPattern = Pattern.compile(term, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
@@ -224,9 +227,9 @@ public final class DialogSearcher extends AbstractSearcher implements Runnable, 
   private Map<StructEntry, StructEntry> makeSearchMap(AbstractStruct struct) {
     final SortedMap<StructEntry, StructEntry> map = new TreeMap<>();
     for (final StructEntry entry : struct.getFields()) {
-      if (entry instanceof AbstractStruct) {
+      if (cbSearchText.isSelected() && entry instanceof AbstractStruct) {
         map.putAll(makeSearchMap((AbstractStruct) entry));
-      } else if (cbsearchcode.isSelected() && entry instanceof AbstractCode) {
+      } else if (cbSearchCode.isSelected() && entry instanceof AbstractCode) {
         map.put(entry, entry);
       } else if (entry instanceof StringRef) {
         map.put(entry, struct);
